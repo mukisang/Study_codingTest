f8d1a2603897fb74821e42c4001002520446f7a5
mukisang <blaxh1101@naver.com>
2019-12-18 12:54:38 +0900

    third commit

--- day05/GA07_lsm.cpp
+++ day05/GA07_lsm.cpp
@@ -8,16 +8,19 @@ using namespace std;
 int main() {
 	int N = 0, M = 0, K = 0, result = 0;
 	cin >> N >> M >> K;
+	//여자수가 남자 2배 보다 많은 경우
 	if (N >= 2 * M)
 	{
 		K -= N - (2 * M);
 		result = M;
 	}
+	//남자 2배가 여자보다 많은 경우
 	else
 	{
 		K -= M - (N/2)+N%2;
 		result = N / 2;
 	}
+	//짝을 짓고도 남은 K를 제거하는 과정
 	if (K > 0)
 	{
 		if (K % 3 == 0)result -= (K / 3);
