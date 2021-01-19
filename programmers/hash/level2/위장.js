d350d00987928b9510d4caa03ded5971fd07c711
Choi Dain <twa05157@gmail.com>
2021-01-20 01:21:22 +0900

    [0120] solve 위장 문제

---
+++ programmers/hash/level2/위장.js
@@ -0,0 +1,6 @@
+function solution(clothes) {
+  return Object.values(clothes.reduce((obj, t) => {
+      obj[t[1]] = obj[t[1]] ? obj[t[1]] + 1 : 1;
+      return obj;
+  }, {} )).reduce((a, b) => a * (b + 1), 1) - 1;
+}
